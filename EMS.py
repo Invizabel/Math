99b51ee0c6f26743fdda7a2a4908a0d37aae28aa
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-03-03 08:01:31 -0600

    Add files via upload

---
+++ EMS.py
@@ -0,0 +1,8 @@
+﻿import os
+
+os.system("clear")
+
+mass = float(input("mass (in grams):\n"))
+diameter = float(input("diameter (in meters)\n"))
+
+print(str(4 / 3 * 3.1415 * ((0.133302e-12 * ((mass / diameter) / (0.266955e-12 * (mass / diameter ** 2)))) / 2) ** 3) + " cubic meters")
---
+++ Gravity.py
@@ -0,0 +1,8 @@
+﻿import os
+
+os.system("clear")
+
+grams = float(input("mass (grams):\n"))
+meters = float(input("diameter (meters):\n"))
+
+print(str(0.266955e-12 * (grams / meters ** 2)) + " meters per second squared")
