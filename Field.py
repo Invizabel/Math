b0b9d17d9346a297cca687fba847ad4117cecd11
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-05-04 00:37:45 -0500

    Update and rename EMS.py to Field.py

---
+++ Field.py
@@ -0,0 +1,9 @@
+﻿import os
+
+# measures the electro-magnetic field or gravitational field of a sphere, not sure which one! :)
+os.system("clear")
+
+mass = float(input("mass (in grams):\n"))
+diameter = float(input("diameter (in meters)\n"))
+
+print(str(4 / 3 * 3.1415 * ((0.133302e-12 * ((mass / diameter) / (0.266955e-12 * (mass / diameter ** 2)))) / 2) ** 3) + " cubic meters")
