97144b031be72c32a7b49efe3de056962267d1ba
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-07-06 20:16:58 -0500

    Update Field.py

--- Field.py
+++ Field.py
@@ -1,9 +1,5 @@
-﻿import os
-
 # measures the electro-magnetic field or gravitational field of a sphere, not sure which one! :)
-os.system("clear")
-
 mass = float(input("mass (in grams):\n"))
 diameter = float(input("diameter (in meters)\n"))
-
-print(str(4 / 3 * 3.1415 * ((0.133302e-12 * ((mass / diameter) / (0.266955e-12 * (mass / diameter ** 2)))) / 2) ** 3) + " cubic meters")
+result = 4 / 3 * 3.1415 * ((0.133302e-12 * ((mass / diameter) / (0.266955e-12 * (mass / diameter ** 2)))) / 2) ** 3
+print(str(result) + " cubic meters")
